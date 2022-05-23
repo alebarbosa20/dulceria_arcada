7b453d1866a1c75a8bde7501af29f377d785e0f7
Alejandra Barbosa <72883499+alebarbosa20@users.noreply.github.com>
2022-05-23 18:01:41 -0500

    Add files via upload

---
+++ conexion.php
@@ -0,0 +1,8 @@
+<?php 
+$host="localhost";
+$username="DeeDeeGtz123";
+$password="Conejo123;123";
+$db_name="dulceria_arcada";
+  
+$conexion= new mysqli ($host,$username,$password, $db_name) or die("Problemas con la conexión en el servidor localhost");
+?>
